ded8196d615acbe300e9673b97cb7e4b64f6d4e2
Muthukumar Gopalakrishnan <muthukumar.g@itp.objectfrontier.com>
2019-04-17 16:34:19 +0530

    updated sql

---
+++ mysql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Persons(PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));
+
+INSERT INTO Persons(PersonID, LastName, FirstName, Address, City) VALUES (4001, 'Muthukumar', 'Gopalakrishnan', '45_FirstAvenue', 'Chennai');
+INSERT INTO Persons(PersonID, LastName, FirstName, Address, City) VALUES (5001, 'Ramkumar', 'Rajavel', '47_FirstAvenue', 'Oakland');
+INSERT INTO Persons(PersonID, LastName, FirstName, Address, City) VALUES (6025, 'Selvaraj', 'Kumaran', '49_FirstAvenue', 'Erode');
